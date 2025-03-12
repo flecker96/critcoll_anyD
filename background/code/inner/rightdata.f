bdb31a7278d45c036aa2fd12d9d2a57d0ab9d471
flecker96 <eckerflorian32@gmail.com>
2025-03-12 10:25:00 +0100

    added order 3 terms in Rtaylor

--- background/code/inner/rightdata.f
+++ background/code/inner/rightdata.f
@@ -13,7 +13,9 @@
      $     u0(nymax), v0(nymax), f0(nymax), ia20(nymax),
      $     u1(nymax), v1(nymax), f1(nymax), ia21(nymax),
      $     u2(nymax), v2(nymax), f2(nymax), ia22(nymax),
-     $     du0dtau(nymax), d2u0dtau2(nymax)
+     $     u3(nymax), v3(nymax), f3(nymax), ia23(nymax),
+     $     du0dtau(nymax), d2u0dtau2(nymax), 
+     $     du2dtau(nymax)
 
 C     Taylor variables     
       double precision f10max, f21max, u10max, u21max,
@@ -96,6 +98,8 @@ C     f2, u2, ia22 are given algebraically
      $           / (32.d0 * ia20(j)**2)
       end do
       
+      call fourdiff1(ny, u2, du2dtau, Delta)
+
 C     Solve a linear inhomogeneous ODE for v2. 
       do j=1,ny
          coeff1(j) = 1.d0 + d / 2.d0 - 2.d0*f1(j) + (3.d0 - d) / ia20(j)
@@ -114,12 +118,95 @@ C     Solve a linear inhomogeneous ODE for v2.
       end do
       call inhom(v2, coeff1, coeff2, Delta, ny)
 
+
+C *** Order 3 ***
+
+      do j=1,ny
+            f3(j) = ((-3.d0 + d)*((1.d0 - f1(j) + f2(j))*ia20(j)**2 + 
+     $             (-1.d0 + f1(j) - f2(j))*ia20(j)**3 + ia21(j)**2 - 
+     $             ia20(j)*((-1.d0 + f1(j))*ia21(j) + ia22(j))))/
+     $         (3.d0*ia20(j)**3)
+            ia23(j) = (-3.d0 + d + (-((ia20(j) - ia21(j) + ia22(j))*
+     $                 (8.d0*(-3.d0 + d) + 
+     $                   (-2.d0 + d)**3*(u0(j)**2 + v0(j)**2))) + 
+     $              2.d0*(-2.d0 + d)**3*(ia20(j) - ia21(j))*
+     $               (u0(j)*u1(j) + v0(j)*v1(j)) - 
+     $              (-2.d0 + d)**3*ia20(j)*
+     $               (u1(j)**2 + 2.d0*u0(j)*u2(j) + v1(j)**2 + 
+     $                 2.d0*v0(j)*v2(j)))/8.d0)/3.d0
+            u3(j) = -(16.d0*(-3.d0 + d)*ia21(j)**2*u0(j) + 
+     $            4.d0*(-3.d0 + d)*ia20(j)*
+     $             ((-3.d0 + d + f1(j)*(-3.d0 + d - 2.d0*ia21(j)) + 
+     $                  6.d0*ia21(j) - 4.d0*ia22(j))*u0(j) - 
+     $               4.d0*ia21(j)*u1(j)) - 
+     $            (-3.d0 + d)*ia20(j)**2*
+     $             (4.d0*(-10.d0 + d + f1(j)*(-1.d0 + d + f1(j)) - 
+     $             2.d0*f2(j))*
+     $                u0(j) + (-2.d0 + d)**3*(1.d0 + f1(j))*u0(j)**3 + 
+     $               2.d0*(-4.d0*(-3.d0 + f1(j))*u1(j) - 8.d0*u2(j) + 
+     $                  (1.d0 + f1(j))*
+     $                   (-2.d0*du0dtau(j) + (-2.d0 + d)*v0(j)))) + 
+     $            ia20(j)**3*(8.d0*du0dtau(j) - 2.d0*d*du0dtau(j) + 
+     $               12.d0*du0dtau(j)*f1(j) - 2.d0*d*du0dtau(j)*f1(j) + 
+     $               4.d0*du0dtau(j)*f1(j)**2 + 
+     $               4.d0*d2u0dtau2(j)*(1.d0 + f1(j)) - 
+     $               8.d0*du0dtau(j)*f2(j) + 
+     $               (-2.d0 + d)*(-16.d0 + d + 
+     $                  f1(j)*(2.d0 + d + 2.d0*f1(j)) - 
+     $                  4.d0*f2(j))*u0(j) - 
+     $               4.d0*(-2.d0 + d)*(-3.d0 + f1(j))*u1(j) 
+     $                  + 16.d0*u2(j) - 
+     $               8.d0*d*u2(j) + 32.d0*v0(j) - 18.d0*d*v0(j) + 
+     $               d**2*v0(j) - 4.d0*f1(j)*v0(j) + 
+     $               d**2*f1(j)*v0(j) - 4.d0*f1(j)**2*v0(j) + 
+     $               2.d0*d*f1(j)**2*v0(j) + 8.d0*f2(j)*v0(j) - 
+     $               4.d0*d*f2(j)*v0(j) - 24.d0*v1(j) + 12.d0*d*v1(j) + 
+     $               8.d0*f1(j)*v1(j) - 4.d0*d*f1(j)*v1(j) 
+     $                  + 16.d0*v2(j) - 
+     $               8.d0*d*v2(j) + 
+     $               16.d0*du2dtau(j)))/
+     $          (96.d0*ia20(j)**3)
+      end do
+
+C     Solve a linear inhomogeneous ODE for v3. 
+      do j=1,ny
+         coeff1(j) = 2.d0 + d / 2.d0 - 3.d0*f1(j) + (3.d0 - d) / ia20(j)
+         
+         coeff2(j) = ((-3.d0 + d)*ia21(j)**3*v0(j))/ia20(j)**4 - 
+     $      ((-3.d0 + d)*ia21(j)*
+     $         (2.d0*ia22(j)*v0(j) + 
+     $           ia21(j)*((-1.d0 + f1(j))*v0(j) + v1(j))))/
+     $       ia20(j)**3 - ((-3.d0 + d)*
+     $         ((-1.d0 + f1(j) - f2(j) + f3(j))*v0(j) + 
+     $           (1.d0 - f1(j) + f2(j))*v1(j) + (-1.d0 + f1(j))*v2(j))
+     $         )/ia20(j) + ((-2.d0 + d)*
+     $          (-1.d0 + f1(j) - f2(j) + f3(j))*u0(j) - 
+     $         (-2.d0 + d)*(-1.d0 + f1(j) - f2(j))*u1(j) + 2.d0*u2(j) - 
+     $         d*u2(j) - 2.d0*f1(j)*u2(j) + d*f1(j)*u2(j) - 
+     $         2.d0*u3(j) + d*u3(j) + 2.d0*v0(j) - d*v0(j) - 
+     $         2.d0*f1(j)*v0(j) + d*f1(j)*v0(j) + 2.d0*f2(j)*v0(j) - 
+     $         d*f2(j)*v0(j) - 2.d0*f3(j)*v0(j) + d*f3(j)*v0(j) - 
+     $         2.d0*v1(j) + d*v1(j) + 2.d0*f1(j)*v1(j) - 
+     $         d*f1(j)*v1(j) - 2.d0*f2(j)*v1(j) + d*f2(j)*v1(j) - 
+     $         2.d0*f3(j)*v1(j) + 
+     $         ((-2.d0 + d)*(-1.d0 + f1(j)) - 4.d0*f2(j))*v2(j))/2.d0 + 
+     $      ((-3.d0 + d)*(ia23(j)*v0(j) + 
+     $           ia22(j)*((-1.d0 + f1(j))*v0(j) + v1(j)) + 
+     $           ia21(j)*((1.d0 - f1(j) + f2(j))*v0(j) + 
+     $              (-1.d0 + f1(j))*v1(j) + v2(j))))/ia20(j)**2
+      end do
+      call inhom(v3, coeff1, coeff2, Delta, ny)
+
 C     Put together the expansion around x=xp=1.
       do j=1,ny
          f(j)  = f0(j) + (x-xp) * f1(j) + (x-xp)**2 * f2(j)
+     $       + (x-xp)**3 * f3(j)
          ia2(j) = ia20(j) + (x-xp) * ia21(j) + (x-xp)**2 * ia22(j)
+     $       + (x-xp)**3 * ia23(j)
          u(j)  = u0(j)+ (x-xp) * u1(j) + (x-xp)**2 * u2(j)
+     $       + (x-xp)**3 * u3(j)
          v(j)  = v0(j)+ (x-xp) * v1(j) + (x-xp)**2 * v2(j)
+     $       + (x-xp)**3 * v3(j)
       end do
 
       if (printtayl) then
